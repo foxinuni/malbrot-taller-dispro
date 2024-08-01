85e2fa7a9901af7e74e8df86e2c58f5f93e45687
Ludayoo <fox@pop-os.tail1f6c9.ts.net>
2024-08-01 11:46:34 -0500

    added multi threading

--- src/cli.c
+++ src/cli.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <pthread.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -12,19 +12,15 @@
 #include "math/complex.h"
 #include "math/matrix.h"
 
-#define WIDTH 15360
-#define HEIGHT 8640
+#define WIDTH 30720
+#define HEIGHT 17280
+#define THREADS 8
 
-int malbrot_converges(compx c, int max_iter) {
-    compx zn = compx_null();
-
-    int i = 0;
-    while(compx_len(zn) <= 2.0 && (i++) < max_iter) {
-        zn = compx_add(compx_sqr(zn), c);
-    }
-
-    return i;
-}
+typedef struct {
+    unsigned char* image;
+    int max_iter;
+    int offset, stride;
+} malbrot_thread_data;
 
 vec3 iteration_to_color(int iteration, int max_iter) {
     double t = (double)iteration / max_iter;
@@ -43,35 +39,92 @@ double pixel_to_complex(int pixel, int size, double min, double max) {
     return min + (max - min) * ((double) pixel / (double) size);
 }
 
+int malbrot_converges(compx c, int max_iter) {
+    compx zn = compx_null();
+
+    int i = 0;  
+    while(compx_len(zn) <= 2.0 && (i++) < max_iter) {
+        zn = compx_add(compx_sqr(zn), c);
+    }
+
+    return i;
+}
+
+void* malbrot_thread(void* data_ptr) {
+    malbrot_thread_data data = *(malbrot_thread_data*) data_ptr;
+
+    for (int pixel = data.offset; pixel < data.stride; pixel++) {
+        int x = pixel % WIDTH;
+        int y = pixel / WIDTH;
+
+        double real = pixel_to_complex(x, WIDTH, -2.5, 1.0);
+        double imag = pixel_to_complex(y, HEIGHT, -1.0, 1.0);
+
+        compx c = compx_new(real, imag);
+
+        int iterations = malbrot_converges(c, data.max_iter);
+        vec3 color = iteration_to_color(iterations, data.max_iter);
+
+        data.image[(y * WIDTH + x) * 3 + 0] = color.r;
+        data.image[(y * WIDTH + x) * 3 + 1] = color.g;
+        data.image[(y * WIDTH + x) * 3 + 2] = color.b;
+    }
+}
+
+void malbrot_trace(unsigned char* image, int max_iter) {
+    pthread_t threads[THREADS];
+    malbrot_thread_data data[THREADS];
+    int pixels_per_thread = WIDTH * HEIGHT / THREADS;
+
+    int pixel_count = 0;
+    for (int i = 0; i < THREADS; i++) {
+        malbrot_thread_data current = {
+            image, max_iter,
+            pixel_count, pixel_count + pixels_per_thread
+        };
+
+        // update state
+        pixel_count += pixels_per_thread;
+        data[i] = current;
+
+        // create thread
+        if (pthread_create(&threads[i], NULL, malbrot_thread, &data[i])) {
+            printf("Error creating thread %d\n", i);
+            exit(-1);
+        }
+
+        printf("Thread %d created (pid: %ld, offset: %d, stride %d)\n", i, threads[i], current.offset, current.stride);
+    }
+
+    // wait for threads to finish
+    for (int i = 0; i < THREADS; i++) {
+        pthread_join(threads[i], NULL);
+        printf("Thread %d finished\n", i);
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         printf("Usage: %s <output_image> <max_iterations>\n", argv[0]);
         return 1;
     }
 
+    // parse arguments
     char* output_image = argv[1];
     int max_iter = atoi(argv[2]);
 
+    // alocate some memory for the image
     unsigned char* image = malloc(WIDTH * HEIGHT * 3);
-    
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            double real = pixel_to_complex(x, WIDTH, -2.5, 1.0);
-            double imag = pixel_to_complex(y, HEIGHT, -1.0, 1.0);
 
-            compx c = compx_new(real, imag);
+    // maldelbrot trace
+    printf("Tracing mandelbrot set with %d iterations\n", max_iter);
+    malbrot_trace(image, max_iter);
 
-            int iterations = malbrot_converges(c, max_iter);
-            vec3 color = iteration_to_color(iterations, max_iter);
-
-            image[(y * WIDTH + x) * 3 + 0] = color.r;
-            image[(y * WIDTH + x) * 3 + 1] = color.g;
-            image[(y * WIDTH + x) * 3 + 2] = color.b;
-        }
-    }
-
+    // write image to disk
+    printf("Writing image to %s\n", output_image);
     stbi_write_png(output_image, WIDTH, HEIGHT, 3, image, WIDTH * 3);
 
+    // free memory
     free(image);
 
     return 0;
